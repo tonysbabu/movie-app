4974a675f957d0537195e7204ef5ae3910ad4856
Wetwer <felix.spoerri@gmail.com>
2019-06-12 16:12:30 +0200

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Home from './components/Home';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div id="top-level-container" className="container h-screen">
         <Home/>
-      </div>
-    );
+  );
   }
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import placeHolder from '../Images/placeholder_for_missing_posters.png'
 
 class Movie extends Component {
@@ -13,19 +13,19 @@ class Movie extends Component {
     }
 
     callback = (entries, observer) => {
-        console.log("entries ",entries);
+        console.log("entries ", entries);
         entries.forEach(entry => {
             console.log(entry);
             if (entry.isIntersecting) {
-              let elem = entry.target;
-        
-              if (entry.intersectionRatio >= 0.5) {
-                  this.setState({imageLoaded: true,});
-                //   elem.src = this.props.movie.image;
-              }
-              observer.unobserve(elem)
+                let elem = entry.target;
+
+                if (entry.intersectionRatio >= 0.5) {
+                    this.setState({imageLoaded: true,});
+                    //   elem.src = this.props.movie.image;
+                }
+                observer.unobserve(elem)
             }
-          });
+        });
     }
 
     componentDidMount() {
@@ -38,30 +38,35 @@ class Movie extends Component {
         // this.observer.observe(document.querySelector(`#image-${this.props.movie.id}`));
         this.observer.observe(this.imageRef);
 
-      
+
     }
 
 
-    handleLoadImage = () =>  {
+    handleLoadImage = () => {
         this.setState({imageVisible: true})
     }
 
     render() {
-        const { name } = this.props.movie;
-        const { imageLoaded, imageVisible } = this.state;
+        const {name} = this.props.movie;
+        const {imageLoaded, imageVisible} = this.state;
         const posterImage = this.props.movie['poster-image'];
         return (
-            <div className="md:w-1/7 sm:w-1/3 text-center  px-1 " >
-                 <div className={`image-container`} ref={(el) => this.imageRef = el}>
-                   {imageLoaded?<img  className={`${imageVisible?'img-loaded':''}`} src={posterImage!='posterthatismissing.jpg'?require(`../Images/${this.props.movie['poster-image']}`):placeHolder} onLoad={this.handleLoadImage}/>:
-                   <img src={placeHolder} />}
+            <div className="card-body">
+                <div className={`image-container`} ref={(el) => this.imageRef = el}>
+                    {imageLoaded ?
+                        <img className={`${imageVisible ? 'img-loaded' : ''}`}
+                             src={posterImage != 'posterthatismissing.jpg' ?
+                                 require(`../Images/${this.props.movie['poster-image']}`) : placeHolder}
+                             onLoad={this.handleLoadImage}/> :
+                        <img src={placeHolder}/>
+                    }
                 </div>
-                <div className="text-white"><h4>{name}</h4></div>
+                <h4 className="text-white">{name}</h4>
             </div>
         );
     }
 }
+
 export default connect((store) => {
-    return {
-    }
-}) (Movie);
+    return {}
+})(Movie);
